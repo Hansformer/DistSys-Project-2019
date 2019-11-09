b549dbc43fc64d374618c2d0f1076e9286876043
Mikael Heino <heinomikael@gmail.com>
2019-11-09 23:51:13 +0200

    Add generic config file
    This should help adding options and changing them without
    modifying code in multiple files.

---
+++ assignment 1/config.py
@@ -0,0 +1,9 @@
+# meta
+LOGLEVEL = 3 # 0-nothing, 1-only errors, 2-normal messages, 3-everything
+SERVERLOG = "./logs/server.txt"
+CLIENTLOG = "./logs/client.txt"
+
+# networking related configurations
+ADDR = "127.0.0.1"
+PORT = 8765
+URI = "ws://localhost:8765"
