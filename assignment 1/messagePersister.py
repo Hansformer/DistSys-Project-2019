0e61011ca22996def6bad989af595eb0ab6964e5
Anton Räihä <avrj@users.noreply.github.com>
2019-11-16 19:59:10 +0200

    Reuse file pointers

--- assignment 1/messagePersister.py
+++ assignment 1/messagePersister.py
@@ -1,15 +1,23 @@
 import os
 import config
 
+filePointers = {}
+
 def getPathForChatRoom(room):
     return os.path.join(config.CHATROOMS_DIR, room)
 
 def saveMessage(room, msg):
-    # TODO: Reuse file pointers
-    roomFile = open(getPathForChatRoom(room), "a+")
+    if room not in filePointers:
+        filePointers[room] = open(getPathForChatRoom(room), "a+")
+
+    roomFile = filePointers[room]
     roomFile.write(msg + "\r\n");
     roomFile.flush();
-    roomFile.close();
+
+# TODO: Close files on exit
+def closeFiles():
+    for file in filePointers:
+        file.close();
 
 def getMessageHistory(room):
     # TODO: Ensure that client can't open any other files that the ones in the config.CHAT dir (by using ../ in file path)
