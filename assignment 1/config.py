e32ae41381522cf66322c485e315946f7c5ee471
Anton Räihä <avrj@users.noreply.github.com>
2019-11-16 17:53:00 +0200

    Remove unused config variables

--- assignment 1/config.py
+++ assignment 1/config.py
@@ -3,10 +3,5 @@ LOGLEVEL = 3 # 0-nothing, 1-only errors, 2-normal messages, 3-everything
 SERVERLOG = "./logs/server.txt"
 CLIENTLOG = "./logs/client.txt"
 
-# networking related configurations
-ADDR = "127.0.0.1"
-PORT = 8765
-URI = "ws://localhost:8765"
-
 # chatrooms
 CHAT = "./chatrooms/"
