7d05df39c8f2320896ad51d460a7f38d8fd8f984
Anton Räihä <avrj@users.noreply.github.com>
2019-11-16 17:57:07 +0200

    Clarify the name of chatroom dir config variable

--- assignment 1/config.py
+++ assignment 1/config.py
@@ -3,5 +3,4 @@ LOGLEVEL = 3 # 0-nothing, 1-only errors, 2-normal messages, 3-everything
 SERVERLOG = "./logs/server.txt"
 CLIENTLOG = "./logs/client.txt"
 
-# chatrooms
-CHAT = "./chatrooms/"
+CHATROOMS_DIR = "./chatrooms/"
--- assignment 1/server.py
+++ assignment 1/server.py
@@ -8,21 +8,23 @@ from logger import Logger
 
 logger = Logger(config.SERVERLOG, config.LOGLEVEL)
 
+def getPathForChatRoom(room):
+    return os.path.join(config.CHATROOMS_DIR, room)
 
 def saveMessage(room, msg):
     # TODO: Reuse file pointers
-    f = open(config.CHAT + room, "a+")
+    f = open(getPathForChatRoom(room), "a+")
     f.write(msg + "\r\n");
     f.flush();
     f.close();
 
 def getMessageHistory(room):
     # TODO: Ensure that client can't open any other files that the ones in the config.CHAT dir (by using ../ in file path)
-    f = open(config.CHAT + room, "rb")
+    f = open(getPathForChatRoom(room), "rb")
     return f.read()
 
 def createDictforRooms():
-    rooms = [f for f in os.listdir(config.CHAT) if os.path.isfile(os.path.join(config.CHAT, f))]
+    rooms = [f for f in os.listdir(config.CHATROOMS_DIR) if os.path.isfile(getPathForChatRoom(f))]
     d = dict()
     for room in rooms:
         d[room] = set()
