a345d33c9ab64c1b50731893d6b1b5150b1bd360
Anton Räihä <avrj@users.noreply.github.com>
2019-11-17 14:34:48 +0200

    Refactor client: rename variables to be more descriptive

--- assignment 1/client.py
+++ assignment 1/client.py
@@ -12,10 +12,10 @@ from logger import Logger
 
 logger = Logger("./logs/client-{}.txt".format(datetime.datetime.now()), config.LOGLEVEL)
 
-async def getNewMessages(socket):
-
+async def receiveMessage(socket):
     async for message in socket:
         logger.logDebug("New message: {}".format(message))
+
         # Chat history is received as bytes
         if type(message) is bytes:
             print(message.decode("utf-8"))
@@ -27,16 +27,20 @@ async def getNewMessages(socket):
 
 
 
-async def sendNewMessages(socket):
+async def sendMessage(socket):
 
     while(True):
         # read message from CLI
-        message = await ainput("Next Message: ")
+        message = await ainput("Message to send: ")
+
         if message == "stop":
             logger.logMsg("Stopping client.")
             break
+
         await socket.send(message)
+
         logger.logMsg("Sent message: {}".format(message))
+
         # tiny sleep so the other corotines can do their thing
         await asyncio.sleep(0.01)
 
@@ -48,43 +52,42 @@ async def chat():
         async with websockets.connect(uri) as websocket:
             logger.logMsg("Connected to server: {}".format(uri))
             # getting chatrooms from server
-            s = await websocket.recv()
-            logger.logMsg("Received message from server: {}".format(s))
+            chatRoomsResponse = await websocket.recv()
+            logger.logMsg("Received message from server: {}".format(chatRoomsResponse))
             
-            chatrooms = s[s.find("[") +1 :s.find("]")].split(", ")
+            chatrooms = chatRoomsResponse[chatRoomsResponse.find("[") +1 :chatRoomsResponse.find("]")].split(", ")
             
             # print chatrooms linewise
             for room in chatrooms:
                 print(room)
             
-            answer = input("Which of the above chatrooms do you want to enter? ")
+            chatRoomToJoin = input("Which of the above chatrooms do you want to join? ")
             
-            if "'" + answer + "'" in chatrooms:
-                await websocket.send(answer)
+            if "'" + chatRoomToJoin + "'" in chatrooms:
+                await websocket.send(chatRoomToJoin)
             else:
                 # break if there is no such chatroom
-                print(answer)
+                print(chatRoomToJoin)
                 print("There is no Chatroom called like that")
                 return
 
             # corotine for recieving messages from server
-            task1 = asyncio.create_task(
-                getNewMessages(websocket)
+            receiveMessageTask = asyncio.create_task(
+                receiveMessage(websocket)
             )
-            # corotine for recieving messages from server
-            task2 = asyncio.create_task(
-                sendNewMessages(websocket)
+            # corotine for sending messages to server
+            sendMessageTask = asyncio.create_task(
+                sendMessage(websocket)
             )
             # start corotines
-            done, pending = await asyncio.wait(
-                [task1, task2],
+            _, pending = await asyncio.wait(
+                [receiveMessageTask, sendMessageTask],
                 return_when=asyncio.FIRST_COMPLETED
             )
             for task in pending:
                 task.cancel()
 
     except websockets.exceptions.ConnectionClosedError:
-        # TODO: log this
         logger.logError("Connection to server lost.")
         print("Unfortenataly we lost connection to Server")
 
